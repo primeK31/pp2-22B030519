16b4c9736d1de019e0893f1b38c850e4f017b17f
primeK31 <ashirbekov005@gmail.com>
2023-01-24 22:22:22 +0600

    Added python files

---
+++ w3school_python/strings/escape_char.py
@@ -0,0 +1,4 @@
+# txt = "We are the so-called "Vikings" from the north."#ERROR
+
+#instead
+txt = "We are the so-called \"Vikings\" from the north."
---
+++ w3school_python/strings/modify.py
@@ -0,0 +1,19 @@
+#upper case
+a = "Hello, World!"
+print(a.upper())
+
+#lower case
+a = "Hello, World!"
+print(a.lower())
+
+#Remove Whitespace
+a = " Hello, World! "
+print(a.strip()) #"Hello, World!"
+
+#Replace String
+a = "Hello, World!"
+print(a.replace("H", "J")) #Jello, World!
+
+#Split String
+a = "Hello, World!"
+print(a.split(",")) #['Hello', ' World!']
