7b8a93db2344a606cc64e8956f07e95c095f52e8
primeK31 <ashirbekov005@gmail.com>
2023-03-08 20:51:07 +0600

    tsis 4

--- tsis4/generators.py
+++ tsis4/generators.py
@@ -2,8 +2,7 @@ import math
 
 def square_generator(n):
     for i in range(1, n + 1):
-        if math.sqrt(i) == int(math.sqrt(i)):
-            yield i
+        yield i ** 2
 
 
 def even_generator(n):
